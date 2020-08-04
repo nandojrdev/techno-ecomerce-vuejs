b6f1164e6cfce778c4dcb5efc24e6a42b2005638
Fernando Junior <fernandojuniordev@gmail.com>
2020-08-04 18:38:45 -0300

    Projeto Atualizado

---
+++ app.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+    }
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(response => response.json())
+        .then(response => {
+          this.produtos = response;
+      })
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
+        })
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.produto = false;
+      }
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+  },
+  created() {
+    this.fetchProdutos();
+  }
+})
